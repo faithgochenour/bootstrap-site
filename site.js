4ff37406e6b487103a5dfe56b8d30b04d0794b48
Brian Gochenour <inbornimperative@gmail.com>
2019-09-28 11:53:18 -0500

    AboutPage changes

---
+++ site.js
@@ -0,0 +1,8 @@
+// $(function() {
+//   for (var i = 0; i < 1000; i++) {
+//     var color = "#" + i * 100;
+//     setTimeout(function() {
+//       $("body").css("background-color", color);
+//     }, 500);
+//   }
+// });
